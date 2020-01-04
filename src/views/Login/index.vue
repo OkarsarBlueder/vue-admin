5dbd9b7414592ef14f33ab2bdc7c0ecaa981f10f
OkarsartBluder <2428825939@qq.com>
2020-01-04 20:25:32 +0800

    'init'

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login">
+        这是登录页面
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login"
+    };
+</script>
+<style scoped lang="scss">
+#login{
+    height: 50vh;
+    background: #344a5f;
+}
+</style>
